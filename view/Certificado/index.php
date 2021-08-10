d2c6fa095681b70d885fef18ccdafeb88ef86b20
Jesus <jesus1.ramirez@ucp.edu.co>
2021-08-10 17:01:11 -0500

    tercerCommit

---
+++ view/Certificado/index.php
@@ -0,0 +1,32 @@
+i<!DOCTYPE html>
+<html lang="es" class="pos-relative">
+  <head>
+    <?php require_once("../html/MainHead.php"); ?>
+    <title>Certificado</title>
+  </head>
+
+  <body class="pos-relative">
+
+    <div class="ht-100v d-flex align-items-center justify-content-center">
+      <div class="wd-lg-70p wd-xl-50p tx-center pd-x-40">
+        <h1 class="tx-100 tx-xs-140 tx-normal tx-inverse tx-roboto mg-b-0">
+            <img src="../../public/certificado.png" class="img-fluid" alt="Responsive image">
+        </h1>
+<br>
+        <p class="tx-16 mg-b-30">
+            The page you are looking for might have been removed, had its name changed,
+            or unavailable. Maybe you could try a search:
+        </p>
+
+        <div class="form-layout-footer">
+            <button class="btn btn-outline-info"><i class="fa fa-download mg-r-10"></i>PNG</button>
+            <button class="btn btn-outline-info"><i class="fa fa-download mg-r-10"></i>PDF</button>
+        </div>
+
+      </div>
+    </div><!-- ht-100v -->
+
+    <?php require_once("../html/MainJs.php"); ?>
+
+  </body>
+</html>
---
+++ view/html/MainMenu.php
@@ -0,0 +1,32 @@
+<!-- ########## START: LEFT PANEL ########## -->
+<div class="br-logo"><a href="../UsuHome/"><span>[</span>YISUS.lta<span>]</span></a></div>
+<div class="br-sideleft overflow-y-auto">
+    <label class="sidebar-label pd-x-15 mg-t-20">Menu</label>
+      <div class="br-sideleft-menu">
+        <a href="../UsuHome/" class="br-menu-link">
+          <div class="br-menu-item">
+            <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
+            <span class="menu-item-label">Inicio</span>
+          </div><!-- menu-item -->
+        </a><!-- br-menu-link -->
+        <a href="../UsuCurso/" class="br-menu-link">
+          <div class="br-menu-item">
+            <i class="menu-item-icon icon ion-ios-bookmarks-outline tx-20"></i>
+            <span class="menu-item-label">Mis cursos</span>
+          </div><!-- menu-item -->
+        </a><!-- br-menu-link -->
+        <a href="../UsuPerfil/" class="br-menu-link">
+          <div class="br-menu-item">
+            <i class="menu-item-icon icon ion-ios-gear-outline tx-24"></i>
+            <span class="menu-item-label">Perfil</span>
+          </div><!-- menu-item -->
+        </a><!-- br-menu-link -->
+
+        <a href="#" class="br-menu-link">
+          <div class="br-menu-item">
+            <i class="menu-item-icon icon ion-power tx-24"></i>
+            <span class="menu-item-label">Cerrar sesion</span>
+          </div><!-- menu-item -->
+        </a>
+    </div><!-- br-sideleft-menu -->
+</div>
